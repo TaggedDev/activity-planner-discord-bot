2ed0eb92153ad71aecc8d9337328fcbf0753a074
Mikov Denis <denis.mikov.06@gmail.com>
2026-05-20 10:17:11 +0300

    Added anime service registration and Microsoft DI, NetCord extensions

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
 ﻿using ActivityPlannerBot.Modules;
+using ActivityPlannerBot.Services.AnimeFetcher;
 using Microsoft.Extensions.Hosting;
 using NetCord.Hosting.Gateway;
 using NetCord.Hosting.Services;
 using NetCord.Hosting.Services.ApplicationCommands;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services
     .AddDiscordGateway()
+    .AddTransient<AnimeParser>()
     .AddApplicationCommands();
+    
+
 
 var host = builder.Build();
 host.AddModules(typeof(AnimeSearchModule).Assembly);
